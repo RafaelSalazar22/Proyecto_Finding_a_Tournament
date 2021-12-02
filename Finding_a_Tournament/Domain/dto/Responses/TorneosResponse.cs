a94d3dca5f242c669511da92f4b7a307b25cea75
RafaelSalazar22 <91817560+RafaelSalazar22@users.noreply.github.com>
2021-12-01 21:33:59 -0600

    Add files via upload

---
+++ Finding_a_Tournament/Domain/dto/Responses/TorneosResponse.cs
@@ -0,0 +1,13 @@
+using System;
+namespace Finding_a_Tournament.Domain.dto.Responses
+{
+    public class TorneosResponse
+    {
+        public string NombreTorneo { get; set; }
+         public int CantidadParticipantes { get; set; }
+
+        public int CantidadEquipos { get; set; }
+        public string TipoTorneo { get; set; }
+        public bool AcepHabilidadesdistintas { get; set; }
+    }
+}
